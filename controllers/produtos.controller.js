276fa3776e357b72fc720254f73fdcf77e76c740
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-30 09:46:23 -0300

    Update produtos.controller.js

--- controllers/produtos.controller.js
+++ controllers/produtos.controller.js
@@ -32,7 +32,7 @@ class ProdutosController {
     }
     await produtosService.create(produto)
     .then(() => {
-      res.send({message: `Musica ${produto.nome} Cadastrada com sucesso`})
+      res.send({message: `Musica ${produto.titulo} Cadastrada com sucesso`})
     })
     .catch((err) => {
       console.error(err);
